f0dc6a2b570ad1a271b05bbcdb9acaec9b211111
clarchiu <clarchiu@gmail.com>
2021-01-25 05:33:56 -0800

    fix basic auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
+  
   def show
-    http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
   end
 end
